e3f8a517d12630e503c23476b3aafb0c2c6bdade
Tom Flynn <thomasjflynn3@gmail.com>
2022-03-13 13:45:45 -0700

    add example uses for scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
+from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
+import random
 
 
 all_data = []
@@ -42,7 +44,7 @@ def get_all_schools():
     return schools_data
 
 # schools_df = pd.DataFrame(schools_data)
-def get_roster(school_link, years=[datetime.now().strftime('%Y') if int(datetime.now().strftime('%m'))>10 else int(datetime.now().strftime('%Y'))-1]):
+def get_roster(school_link, years=[datetime.now().strftime('%Y') if int(datetime.now().strftime('%m'))<10 else int(datetime.now().strftime('%Y'))+1]):
 
     # Initialize roster data
     roster_data = []
@@ -50,10 +52,11 @@ def get_roster(school_link, years=[datetime.now().strftime('%Y') if int(datetime
     # Get seasons
     for year in years:
         print(f"Season: {year}")
-        test_url = f"{school_link}{year}.html"
+        season_roster_url = f"{school_link}{year}.html"
 
+        print(season_roster_url)
         # Get Team Roster page
-        players_html = requests.get(base_url + test_url).text
+        players_html = requests.get(base_url + season_roster_url).text
 
         # Parse Roster table rows
         roster_table = BeautifulSoup(players_html, 'lxml').find("table", {"id": "roster"})
@@ -93,23 +96,58 @@ def get_player_gamelog(player_link):
 
     # Parse gamelog table rows
     gamelog_table = BeautifulSoup(gamelog_html, 'lxml').find("table", {"id": "gamelog"})
-    player_games = gamelog_table.find_all('tr')
 
-    # Initialize gamelog array
-    gamelog = []
+    # Some players have not played any games
+    # Check for the gamelog table before getting rows
+    if gamelog_table:
+        player_games = gamelog_table.find_all('tr')
 
-    # Loop through each gamelog row
-    for player_game in player_games:
+        # Initialize gamelog array
+        gamelog = []
 
-        # Initialize game data dict
-        player_game_data_temp = {}
+        # Loop through each gamelog row
+        for player_game in player_games:
 
-        # Loop through gamelog row's cells
-        for player_game_data in player_game.find_all('td'):
-            data_type = player_game_data['data-stat']
-            data = player_game_data.text
-            player_game_data_temp[data_type] = data
+            # Initialize game data dict
+            player_game_data_temp = {}
 
-        gamelog.append(player_game_data_temp)
+            # Loop through gamelog row's cells
+            for player_game_data in player_game.find_all('td'):
+                data_type = player_game_data['data-stat']
+                data = player_game_data.text
+                player_game_data_temp[data_type] = data
+
+            gamelog.append(player_game_data_temp)
     
     return gamelog
+
+
+if __name__ == "__main__":
+
+    # Create a schools dataframe
+    s = get_all_schools()
+    schools_df = pd.DataFrame(s)
+    print(schools_df.head())
+
+    # Get a random school
+    random_school = schools_df.sample().to_dict('records')[0] 
+    # Check if school played this year
+    while random_school['year_max'] != '2022':
+        random_school = schools_df.sample().to_dict('records')[0]
+
+    print(f'Randomly selected school: {random_school["school_name"]}')
+
+    # Get team roster
+    r = get_roster(random_school["school_link"])
+    roster_df = pd.DataFrame(r)
+    print(roster_df.head())
+
+    # Get a random player
+    random_player = roster_df.sample().to_dict('records')[0]
+    print(f'Randomly selected player: {random_player["player"]}')
+    
+    # Get player game log
+    p = get_player_gamelog(random_player['player_link'])
+    # Player gamelog has some empty rows
+    gamelog_df = pd.DataFrame(p).dropna()
+    print(gamelog_df.head())
